27649fcfcd05039f9d9c92f1dc080d47380d0551
Bretton Scarbrough <bretton.scarbrough@gmail.com>
2024-12-29 18:24:00 -0600

    Changed favicons

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -252,7 +252,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "p18v8e"
+  version_hash: "1o1fjy2"
 };
 function get_hooks() {
   return {};
